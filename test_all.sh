6e85d8ead837b307bbe15f85212cddf18084c7b1
mallorbc <mallorbc@mail.uc.edu>
2019-05-05 12:06:12 -0400

    started refactoring parser, parse program has been refactored; Added testing script to repo

---
+++ test_all.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#cleans build then rebuilds
+echo "Will compile then test"
+sleep 1
+make clean
+make
+sleep 1
+clear
+
+#moves compiler binary to testPgms/correct and then starts testing
+yes | cp compiler testPgms/correct
+cd testPgms/correct
+
+echo "Testing provided programs"
+sleep 2
+clear
+
+echo "Testing Math.src"
+sleep 1
+echo $(./compiler math.src)
+sleep 1
+clear
+
+echo "Testing test_heap.src"
+sleep 1
+echo $(./compiler test_heap.src)
+sleep 1
+clear
+
+echo "Testing test_program_minimal.src"
+sleep 1
+echo $(./compiler test_program_minimal.src)
+sleep 1
+clear
+
+echo "Testing test2.src"
+sleep 1
+echo $(./compiler test2.src)
+sleep 1
+clear
+
+echo "Testing logicals.src"
+sleep 1
+echo $(./compiler logicals.src)
+sleep 1
+clear
+
+echo "Testing multipleProcs.src"
+sleep 1
+echo $(./compiler multipleProcs.src)
+sleep 1
+clear
+
+echo "Testing source.src"
+sleep 1
+echo $(./compiler source.src)
+sleep 1
+clear
+
+echo "Testing test1.src"
+sleep 1
+echo $(./compiler test1.src)
+sleep 1
+clear
+
+echo "Testing iterativeFib.src"
+sleep 1
+echo $(./compiler iterativeFib.src)
+sleep 1
+clear
+
+echo "Testing recursiveFib.src"
+sleep 1
+echo $(./compiler recursiveFib.src)
+sleep 1
+clear
+
+echo "Testing test1b.src; Should fail with period error"
+sleep 2
+echo $(./compiler test1b.src)
+sleep 1
+clear
+
+echo "All 11 given tests have been tested"
